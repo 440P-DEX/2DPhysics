5e5d90f6f95b5f73f0e44ecb56bc76df39f1a7d6
440P-DEX <khung250@gmail.com>
2024-05-28 00:03:16 +0700

    Added new folder "source" and added all the implementation files in.

---
+++ 2DPhysics/source/Application.cpp
@@ -0,0 +1,139 @@
+#include "Application.hpp"
+
+#define FORLOOPCIRCLE for (const auto& circle : circles)
+
+std::unique_ptr<Application> Application::Instance = std::make_unique<Application>();
+
+Application::Application() = default;
+Application::~Application() = default;
+
+sf::Vector2f Application::calculateMomentum()
+{
+	sf::Vector2f velocity;
+
+	if (mouseTrajectory.size() > 10)
+	{
+		velocity.x = (mouseTrajectory[mouseTrajectory.size() - 1].x - mouseTrajectory.front().x) / static_cast<float>(mouseTrajectory.size()) * 2;
+		velocity.y = (mouseTrajectory[mouseTrajectory.size() - 1].y - mouseTrajectory.front().y) / static_cast<float>(mouseTrajectory.size()) * 2;
+	}
+
+	return velocity;
+}
+
+void Application::handleEvents()
+{
+	sf::Event event;
+
+	while (window->pollEvent(event))
+	{
+		switch (event.type)
+		{
+		case sf::Event::Closed:
+			std::cout << "Window closed...!\n";
+			window->close();
+			break;
+
+		case sf::Event::GainedFocus:
+			std::cout << "Window gained focus.\n";
+			break;
+		
+		case sf::Event::LostFocus:
+			std::cout << "Window lost focus.\n";
+			break;
+
+		case sf::Event::MouseEntered:
+			std::cout << "Mouse entered the window.\n";
+			break;
+
+		case sf::Event::MouseLeft:
+			std::cout << "Mouse left the window.\n";
+			break;
+
+		case sf::Event::MouseButtonPressed:
+			if (event.mouseButton.button == sf::Mouse::Left)
+			{
+				FORLOOPCIRCLE
+				{
+					circle->onMousePressed(sf::Vector2f(static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y)));
+					if (circle->isBeingDragged()) break;
+				}
+
+			}
+			break;
+
+		case sf::Event::MouseButtonReleased:
+			if (event.mouseButton.button == sf::Mouse::Left)
+			{
+				FORLOOPCIRCLE circle->onMouseReleased(calculateMomentum());
+			}
+			break;
+
+		case sf::Event::MouseMoved:
+			FORLOOPCIRCLE circle->onMouseMoved(sf::Vector2f(static_cast<float>(event.mouseMove.x), static_cast<float>(event.mouseMove.y)));
+			break;
+
+		default:
+			break;
+		}
+	}
+}
+
+void Application::update()
+{
+	mouseTrajectory.push_back(static_cast<sf::Vector2f>(sf::Mouse::getPosition(*window)));
+	if (mouseTrajectory.size() > 20) { mouseTrajectory.erase(mouseTrajectory.begin()); }
+}
+
+void Application::render()
+{
+	window->clear();
+
+	for (const auto& circle : circles)
+	{
+		window->draw(circle->getObj());
+	}
+
+	window->display();
+}
+
+void Application::run()
+{
+	while (window->isOpen())
+	{
+		FORLOOPCIRCLE circle->update();
+
+		Application::update();
+		Application::handleEvents();
+		Application::render();
+	}
+}
+
+void Application::init(unsigned width, unsigned height, const std::string& title, int number, float radius, unsigned limit)
+{
+	initWindow(width, height, title, limit);
+	initObject(number, radius);
+
+	std::cout << "Subsystem initialized...!\n";
+}
+
+void Application::initWindow(unsigned width, unsigned height, const std::string& title, unsigned limit)
+{
+	window = std::make_unique<sf::RenderWindow>();
+
+	sf::VideoMode mode;
+	mode.width = width;
+	mode.height = height;
+
+	window->create(mode, title, sf::Style::Close);
+	window->setFramerateLimit(limit);
+}
+
+void Application::initObject(int number, float radius)
+{
+	auto size = static_cast<sf::Vector2f>(window->getSize());
+
+	for (int i = 0; i < number; ++i)
+	{
+		circles.push_back(std::make_unique<Circle>(radius, randomColor(), sf::Vector2f(randomFloat(radius, size.x - radius), randomFloat(radius, size.y - radius))));
+	}
+}
---
+++ 2DPhysics/source/Utility.cpp
@@ -0,0 +1,30 @@
+#include "Utility.hpp"
+#include <random>
+
+float randomFloat(float min, float max)
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+
+	std::uniform_real_distribution<float> dis(min, max);
+
+	return dis(gen);
+}
+
+unsigned int randomInt(unsigned int min, unsigned int max)
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+
+	std::uniform_int_distribution<int> dis(min, max);
+
+	return dis(gen);
+}
+
+sf::Color randomColor()
+{
+	auto r = static_cast<sf::Uint8>(randomInt(0, 255));
+	auto g = static_cast<sf::Uint8>(randomInt(0, 255));
+	auto b = static_cast<sf::Uint8>(randomInt(0, 255));
+	return sf::Color(r, g, b);
+}
