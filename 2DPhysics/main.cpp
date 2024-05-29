9e6feae08c2a3267680afcb5d1be8dc2cd2c6119
440P <k.hung250vn@gmail.com>
2024-05-29 12:15:55 +0700

    Update the main function.

--- 2DPhysics/main.cpp
+++ 2DPhysics/main.cpp
@@ -3,8 +3,10 @@
 
 int main()
 {
+	// Set `RADIUS` to any (non-negative) value you want, and if you want random sizes for each object, set 'RADIUS' to 0.
 	Application::Instance->init(WINDOW_WIDTH, WINDOW_HEIGHT, "2D Physics", 5U, RADIUS, 60);
 
+	// Run the instance of Application.
 	Application::Instance->run();
 
 	return EXIT_SUCCESS;
