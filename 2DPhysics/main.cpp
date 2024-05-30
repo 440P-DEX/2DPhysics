2597ec6d6d57d71b61d30eadf109b9355003b8c7
440P <k.hung250vn@gmail.com>
2024-05-30 12:39:25 +0700

    Add a few comments.

--- 2DPhysics/main.cpp
+++ 2DPhysics/main.cpp
@@ -3,8 +3,10 @@
 
 int main()
 {
-	// Set `RADIUS` to any (non-negative) value you want, and if you want random sizes for each object, set 'RADIUS' to 0.
-	Application::Instance->init(WINDOW_WIDTH, WINDOW_HEIGHT, "2D Physics", 5U, RADIUS, 60);
+	// Set `RADIUS` to any non-negative real value you want, and if you want random sizes for each object, set 'RADIUS' to 0. Default value for RADIUS is 50.0.
+	// Set 'OBJECTS' to any non-negative integer value you want (0 is valid value as it is non-negative). Default value for OBJECTS is 5.
+	// Set 'FPS' to any non-negative integer value you want.
+	Application::Instance->init(WINDOW_WIDTH, WINDOW_HEIGHT, "2D Physics", OBJECTS, RADIUS, FPS);
 
 	// Run the instance of Application.
 	Application::Instance->run();
