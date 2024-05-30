27cb4fd3df22ebe032d648bc9768551de68a1da8
440P <k.hung250vn@gmail.com>
2024-05-30 12:38:46 +0700

    Add more constants.

--- 2DPhysics/include/Constants.hpp
+++ 2DPhysics/include/Constants.hpp
@@ -1,8 +1,19 @@
+/*
+This is the header for constants, you can change value here.
+Also it is recommended to change value here.
+*/
+
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
 
 // Radius of the circles.
-constexpr float RADIUS = 50.f;
+constexpr float RADIUS = 50.0F;
+
+// The frame rate limit.
+constexpr unsigned FPS = 60;
+
+// The number of the Circle objects to generate.
+constexpr unsigned OBJECTS = 5U;
 
 // Width of the window.
 constexpr unsigned WINDOW_WIDTH = 800U;
@@ -18,5 +29,11 @@ constexpr float retention = 0.8f;
 
 // Friction for reducing horizontally moving.
 constexpr float friction = 0.99f;
+
+// Perfectly elastic collision.
+constexpr float restitution = 1.0F;
+
+// volumetric mass density or specific mass of the circle
+constexpr float density = 1.0F;
 
 #endif // !CONSTANTS_HPP
